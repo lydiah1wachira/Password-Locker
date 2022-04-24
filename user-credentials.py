5d819168d450f9112fdbec673b8cc9f319240c5c
lydiah1wachira <lydiahcedric@gmail.com>
2022-04-24 11:02:56 +0300

    create a class variable for the user list and instance variables for new instances of the user class.

--- user-credentials.py
+++ user-credentials.py
@@ -4,3 +4,11 @@ class User:
   '''
   Class user that generates new instances of a user
   '''
+  user_list = []
+
+  def __init__(self, username, password):
+    '''
+    __init__ method that helps define properties of a user object
+    '''
+    self.username = username
+    self.password = password
