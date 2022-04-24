5f8b1ba33192c80877a6ae87040434fbfdfdbfad
lydiah1wachira <lydiahcedric@gmail.com>
2022-04-24 12:27:38 +0300

    create a test method to check if objects are being initialized properly.

--- passlock_test.py
+++ passlock_test.py
@@ -5,3 +5,18 @@ class TestClass(unittest.TestCase):
   '''
   Test class that defines test cases for the User class behaviours.
   '''
+  def setUp(self):
+    '''
+    set up method to run before each individual test case
+    '''
+    self.new_user = User('Severus Snape', 'Expelliarmus')
+
+  def test_init(self):
+    '''
+    test case to check if object is being initialized correctly
+    '''
+    self.assertEqual(self.new_user.username,'Severus Snape')
+    self.assertEqual(self.new_user.password,'Expelliarmus' )
+
+if __name__ == '__main__':
+    unittest.main()
