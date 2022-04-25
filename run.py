9d2600f2386c49c704b0758865de9aeefeb89835
lydiah1wachira <lydiahcedric@gmail.com>
2022-04-25 23:25:24 +0300

    create functions in the run.py file.

--- run.py
+++ run.py
@@ -16,4 +16,30 @@ def save_user(user):
   '''
   user.save_user()
 
+def login_user(username,password):
+  '''
+  Function that verifies whether a user exists and then logs them in 
+  '''
+  confirm_user = Credentials.verify_user(username,password)
+  return confirm_user
+
+def create_new_credential(account, username, password):
+  '''
+  function that creates new credentials for a specific user account
+  '''
+  new_credentials = Credentials(account,username,password)
+  return new_credentials
+
+def save_details(credentials):
+  '''
+  Function to save credentials to the credentials list
+  '''
+  credentials.save_credentials()
+
+def del_credentials(credentials):
+  '''
+  Function to delete credentials from the credentials list
+  '''
+  credentials.delete_credentials()
+
 def 
